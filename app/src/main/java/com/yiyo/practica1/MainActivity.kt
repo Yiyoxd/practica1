73b23d49980dff47f54d5774dde3afcd53388597
Alfredo Alberto Palacios Rodriguez <yiyoaapr@gmail.com>
2026-02-04 11:20:42 -0600

    Terminado

--- app/src/main/java/com/yiyo/practica1/MainActivity.kt
+++ app/src/main/java/com/yiyo/practica1/MainActivity.kt
@@ -4,18 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yiyo.practica1.ui.theme.Practica1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /*
         enableEdgeToEdge()
         setContent {
             Practica1Theme {
@@ -27,6 +38,38 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        */
+
+        setContent {
+            Practica1Theme() {
+                pantallaPrincipal()
+            }
+        }
+    }
+}
+
+@Composable
+fun pantallaPrincipal() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Alfredo Alberto Palacios Rodriguez",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Blue,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(
+            text = "22130550",
+            fontSize = 18.sp,
+            color = Color.DarkGray
+        )
     }
 }
 
